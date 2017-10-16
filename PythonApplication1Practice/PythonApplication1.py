e8a2f7cf16f3da8c6b64e07a3bb68c40c3fbb82b
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-10-15 22:24:03 -0400

    Add project files.

---
+++ PythonApplication1Practice/PythonApplication1.py
@@ -0,0 +1,9 @@
+
+print ('Hello, world!')
+
+name = input('What is your name?\n')
+print('Hi, %s.' % name)
+
+friends = ['traci', 'maggie', 'gary', 'michael', format.__name__]
+for i, name in enumerate(friends):
+    print("iteration {iteration} is {name}".format(Iteration=i, name=name))
